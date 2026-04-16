e2595e12b1397b97d710dc6cc21837acb695e60d
Macie <Macie@MSI>
2026-04-16 12:48:57 -0400

    modified the cntrl point set up in inputs

--- inputs.m
+++ inputs.m
@@ -6,7 +6,7 @@
 % *** Input values in this section ***
 rotor.numBlades = 3; % Input desired number of blades
 rotor.R = 1; % Input total rotor radius in m
-rotor.numStns = 11; % Input number of blade stations
+rotor.numCntrlPnts = 10; % Input desired number of cntrl points for calculations
 rotor.TSR = 7.5; % Input rotor tip speed ratio
 
 %% User inputs for working fluid
@@ -15,8 +15,9 @@ wf.WindSpeed = 10; % Input free stream velocity in m/s
 
 %% Additional set up calculations
 % *** DO NOT ALTER FORMULAS ***
-rotor.stnCntrlPnt = linspace(0, rotor.R, rotor.numStns);
-rotor.Area = pi * rotor.R ^ 2; % Swept area of rotor
+rotor.stnEdges = linspace(0, rotor.R, rotor.numCntrlPnts+1);
+rotor.CntrlPntDist = (rotor.stnEdges(1:end-1) + rotor.stnEdges(2:end))/2;
+rotor.Area = pi * rotor.R ^ 2; % Swept area
 rotor.RotationalSpeed = (rotor.TSR * wf.WindSpeed) / rotor.R ; % In rad/s
 wf.Density = 1.225; 
 wf.KinematicVisc = 1.789e-5; % Std air
